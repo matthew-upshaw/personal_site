6d5483bfe9e037f404e91ac27dd2e88172175e05
matthew-upshaw <matthew.upshaw02@gmail.com>
2023-02-16 12:05:57 -0600

    update model fields

--- website/admin.py
+++ website/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 from . models import *
 
 admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(Publication)
--- website/models.py
+++ website/models.py
@@ -5,14 +5,37 @@ class WorkExperience(models.Model):
     description = models.TextField(max_length=5000)
     employer = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
     date_started = models.DateField()
-    date_left = models.DateField()
+    date_left = models.DateField(blank=True, null=True)
+    current_employer = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.title} - {self.employer}'
 
 class Education(models.Model):
-    pass
+    school = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    major = models.CharField(max_length=255)
+    minor = models.CharField(max_length=255, blank=True, null=True)
+    gpa = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    date_started = models.DateField()
+    graduation_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.school} - {self.degree}'
+
 
 class Publication(models.Model):
-    pass
+    title = models.CharField(max_length=510)
+    abstract = models.TextField(max_length=5000)
+    journal = models.CharField(max_length=510)
+    year = models.CharField(max_length=255)
+    number_of_authors = models.IntegerField()
+    authors = models.CharField(max_length=510)
+    #feature_image = models.ImageField()
+
+    def __str__(self):
+        return f'{self.title}'
