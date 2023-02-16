e99a55191bc218d14fec8fb780a9b3a11d6214ee
matthew-upshaw <matthew.upshaw02@gmail.com>
2023-02-16 09:06:10 -0600

    update ALLOWED_HOSTS in settings.py

--- matthewtupshaw/settings.py
+++ matthewtupshaw/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
-ALLOWED_HOSTS = [env('IP_ADDRESS')]
+ALLOWED_HOSTS = [env('IP_ADDRESS'), 'localhost']
 
 
 # Application definition
